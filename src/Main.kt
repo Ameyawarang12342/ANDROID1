1d024a67ee6e1c5ffe4cacd999de0cb5240cc0aa
ameya <ameyawarang203@gmail.com>
2024-05-24 04:15:06 +0530

    compact function use case

--- src/Main.kt
+++ src/Main.kt
@@ -4,10 +4,12 @@ import com.kotlin1.fileThree.waterChange
 import com.kotlin1.fileOne.randomDay
 import com.kotlin1.fileTwo.food
 
+
+
 fun main(args: Array<String>) {
     val day : String = randomDay()
     val fishfood :String = food(day)
-    val change : Boolean = waterChange(22,day,34)
+    val change : Boolean = waterChange(32,day,34)
 
     return println("ON $day fish are fed $fishfood\n waterchange $change" )
 
--- src/com/example/waterChange.kt
+++ src/com/example/waterChange.kt
@@ -2,12 +2,15 @@ package com.kotlin1.fileThree
 
 import com.kotlin1.fileOne.randomDay
 
+
 fun waterChange(temp : Int , day :String, dirt : Int ): Boolean
     {
+    fun temp2 (temp:Int) = temp > 30
         return when {
-            temp > 50 -> true
+            temp2(temp) -> true
             day == "sun" -> true
             dirt > 50 -> true
             else -> false
-        }
+
     }
+}
